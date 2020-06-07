1b6f0b0ebc02ef4d7dc4061679704800c1c057a1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-06-07 14:48:30 -0300

    EhbrsRubyUtils::Videos::Resolution.

---
+++ lib/ehbrs_ruby_utils/videos/resolution.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'ehbrs_ruby_utils/videos/quality'
+
+module EhbrsRubyUtils
+  module Videos
+    class Resolution
+      enable_simple_cache
+      include ::Comparable
+
+      class << self
+        def valid_dimension?(dimension)
+          dimension.is_a?(::Integer) && dimension.positive?
+        end
+      end
+
+      common_constructor :width, :height
+
+      def <=>(other)
+        r = (lower <=> other.lower)
+        return r unless r.zero?
+
+        higher <=> other.higher
+      end
+
+      def to_xs
+        [quality_to_s, resolution_to_s].join(' / ')
+      end
+
+      def higher
+        width > height ? width : height
+      end
+
+      def lower
+        width < height ? width : height
+      end
+
+      def pixels
+        width * height
+      end
+
+      def quality_to_s
+        quality.if_present('?', &:to_s)
+      end
+
+      def resolution_to_s
+        "#{width}x#{height}"
+      end
+
+      def to_s
+        resolution_to_s
+      end
+
+      def valid?
+        [width, height].all? { |d| self.class.valid_dimension?(d) }
+      end
+
+      private
+
+      def quality_uncached
+        ::EhbrsRubyUtils::Videos::Quality.list
+          .find { |quality| quality.resolution_match?(self) } ||
+          raise("No quality matches resolution #{resolution}")
+      end
+    end
+  end
+end
