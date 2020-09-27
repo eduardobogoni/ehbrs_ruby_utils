1d277b55982a795a8239dd7a42ef0fd7427d164d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-09-27 15:44:41 -0300

    EhbrsRubyUtils::Executables.

---
+++ lib/ehbrs_ruby_utils/executables.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/envs'
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Executables
+    class << self
+      enable_simple_cache
+
+      def env
+        ::EacRubyUtils::Envs.local
+      end
+
+      private
+
+      {
+        '-version' => %w[ffprobe]
+      }.each do |validate_arg, commands|
+        commands.each do |command|
+          define_method("#{command}_uncached") do
+            env.executable(command, validate_arg)
+          end
+        end
+      end
+    end
+  end
+end
