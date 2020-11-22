d081adcaaebd1c1a9dbcb79cac290a7834f1e7d4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-22 20:02:37 -0300

    EhbrsRubyUtils::Videos::Stream.

---
+++ lib/ehbrs_ruby_utils/videos/stream.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Videos
+    class Stream
+      enable_simple_cache
+      enable_listable
+
+      lists.add_symbol :codec_type, :audio, :video, :subtitle, :other
+
+      common_constructor :ffprobe_data do
+        self.ffprobe_data = ffprobe_data.symbolize_keys.freeze
+        self.class.lists.codec_type.value_validate!(codec_type)
+      end
+
+      def to_s
+        "#{index}|#{codec_type}|#{codec_name}|#{language}"
+      end
+
+      def to_h
+        ffprobe_data
+      end
+
+      %i[index codec_name codec_long_name].each do |method_name|
+        define_method method_name do
+          ffprobe_data.fetch(method_name)
+        end
+      end
+
+      def codec_type
+        ffprobe_data.fetch(:codec_type).to_sym
+      end
+
+      def tags
+        ffprobe_data.fetch(:tags).symbolize_keys
+      end
+
+      def language
+        tags[:language]
+      end
+    end
+  end
+end
