8ffa48e0e0831e04780834321932f0671cf053b2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-01 00:00:31 -0300

    EhbrsRubyUtils::Videos::Container: absorve EhbrsRubyUtils::Videos::Container::Info.

--- lib/ehbrs_ruby_utils/videos/container.rb
+++ lib/ehbrs_ruby_utils/videos/container.rb
@@ -2,7 +2,6 @@
 
 require 'eac_ruby_utils/core_ext'
 require 'ehbrs_ruby_utils/executables'
-require 'ehbrs_ruby_utils/videos/container/info'
 require 'ehbrs_ruby_utils/videos/stream'
 require 'json'
 
@@ -20,24 +19,18 @@ module EhbrsRubyUtils
         end
       end
 
-      def probe_data
-        info.to_h.deep_symbolize_keys
-      end
-
       private
 
-      def info_uncached
-        ::EhbrsRubyUtils::Videos::Container::Info.new(
-          ::JSON.parse(
-            ::EhbrsRubyUtils::Executables.ffprobe.command(
-              '-hide_banner', '-print_format', 'json', '-show_format', '-show_streams', path
-            ).execute!
-          )
-        )
+      def probe_data_uncached
+        ::JSON.parse(
+          ::EhbrsRubyUtils::Executables.ffprobe.command(
+            '-hide_banner', '-print_format', 'json', '-show_format', '-show_streams', path
+          ).execute!
+        ).deep_symbolize_keys.freeze
       end
 
       def streams_uncached
-        info.ffprobe_data.fetch(:streams).map do |stream_ffprobe_data|
+        probe_data.fetch(:streams).map do |stream_ffprobe_data|
           ::EhbrsRubyUtils::Videos::Stream.new(stream_ffprobe_data)
         end
       end
