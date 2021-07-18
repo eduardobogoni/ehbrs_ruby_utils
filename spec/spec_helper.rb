8b045392751add32dd9539ae392286f0ec5a156e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-07-18 12:50:50 -0300

    spec/**/*.rb: adequa a "eac_ruby_gem_support" '0.2.0'.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  require 'eac_ruby_gem_support/rspec'
+  ::EacRubyGemSupport::Rspec.setup(::File.expand_path('..', __dir__), config)
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
