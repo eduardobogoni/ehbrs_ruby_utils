f8aab134bdf3f2046b41db2de83dfd7927e53d18
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-12-04 21:25:57 -0300

    EhbrsRubyUtils::WebUtils::Instance::Finances::Bills.

---
+++ lib/ehbrs_ruby_utils/web_utils/instance/finances/bills.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'avm/instances/base'
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module WebUtils
+    class Instance < ::Avm::Instances::Base
+      class Finances
+        class Bills
+          require_sub __FILE__
+          common_constructor :finances
+          delegate :instance, to: :finances
+
+          # @return [Pathname]
+          def bills_directory(suffix)
+            instance.read_entry('finances.bills.directory').to_pathname.join(suffix)
+          end
+
+          # @return [EhbrsRubyUtils::WebUtils::Instance::Finances::Bills::Consume]
+          def consume
+            ::EhbrsRubyUtils::WebUtils::Instance::Finances::Bills::Consume.new(self)
+          end
+
+          # @return [Pathname]
+          def pending_directory
+            bills_directory('pending')
+          end
+
+          # @return [Pathname]
+          def registered_directory
+            bills_directory('registered')
+          end
+        end
+      end
+    end
+  end
+end
