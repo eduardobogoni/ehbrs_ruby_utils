db012e884f0f7afd5e8ebd241f84ec92bad5576d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-02-12 16:50:02 -0300

    EhbrsRubyUtils::Videos::Series::Rename::File::Renamer: adiciona versão a nomes de arquivos que já existem.

--- lib/ehbrs_ruby_utils/videos/series/rename/file/renamer.rb
+++ lib/ehbrs_ruby_utils/videos/series/rename/file/renamer.rb
@@ -8,15 +8,40 @@ module EhbrsRubyUtils
       module Rename
         class File
           class Renamer
+            enable_speaker
             common_constructor :owner
-            delegate :dirname, :file, :new_name, to: :owner
+            delegate :current_name, :dirname, :file, :new_name, to: :owner
 
             def perform
-              target = dirname.join(new_game)
-              return if target.exist?
+              return unless rename?
 
+              version_number = 0
+              loop do
+                target_path = dirname.join(build_target_name(version_number))
+                break perform_rename(target_path) unless target_path.exist?
+
+                version_number += 1
+              end
+            end
+
+            private
+
+            def build_target_name(version_number)
+              return new_name unless version_number.positive?
+
+              extname = ::File.extname(new_name)
+              "#{::File.basename(new_name, extname)}.v#{version_number}#{extname}"
+            end
+
+            def perform_rename(target_path)
               infov 'Renaming', file.to_path
-              ::FileUtils.mv(file.to_path, target.to_path)
+              ::FileUtils.mv(file.to_path, target_path.to_path)
+            end
+
+            def rename?
+              return false unless new_name
+
+              current_name != new_name
             end
           end
         end
