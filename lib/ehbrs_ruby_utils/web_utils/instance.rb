b3d0bd0fd298906ffa9e4e5f5f9b23f7cd26e6ef
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-21 02:18:10 -0300

    EhbrsRubyUtils::WebUtils::Instance: substitui "httpclient" por "eac_rest".

--- lib/ehbrs_ruby_utils/web_utils/instance.rb
+++ lib/ehbrs_ruby_utils/web_utils/instance.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
+require 'eac_rest/api'
 require 'eac_ruby_utils/core_ext'
 require 'avm/instances/base'
-require 'httpclient'
 
 module EhbrsRubyUtils
   module WebUtils
     class Instance < ::Avm::Instances::Base
       require_sub __FILE__
+
+      # @return [EacRest::Api]
+      def api
+        ::EacRest::Api.new(root_url, read_entry(:admin_username), read_entry(:admin_password))
+      end
 
       def finances
         @finances ||= ::EhbrsRubyUtils::WebUtils::Instance::Finances.new(self)
@@ -17,21 +22,26 @@ module EhbrsRubyUtils
         read_entry(::Avm::Instances::EntryKeys::WEB_URL)
       end
 
-      def resource_url(resource_url_suffix)
-        root_url + '/' + resource_url_suffix.gsub(%r{\A/+}, '')
-      end
-
+      # @param resource_url_suffix [String]
+      # @param options [Hash]
+      # @return [EacRest::Response]
       def http_request(resource_url_suffix, options = {})
-        method = options.delete(:method) || 'get'
-        url = resource_url(resource_url_suffix)
-        http_client.request(method, url, options)
+        options = http_request_options(options)
+        r = api.request(resource_url_suffix).verb(options.verb).headers(options.headers)
+              .body_data(options.body_data).response
+        ::Struct.new(:status, :body).new(r.status, r.body_str)
       end
 
-      def http_client_uncached
-        client = HTTPClient.new
-        client.force_basic_auth = true
-        client.set_basic_auth(root_url, read_entry(:admin_username), read_entry(:admin_password))
-        client
+      private
+
+      # @return [::Struct.new(:verb, :body_data, :headers)]
+      def http_request_options(options)
+        options = options.to_options_consumer
+        verb = options.consume(:method, :get).to_sym
+        body_data = options.consume(:body)
+        headers = options.consume(:header)
+        options.validate
+        ::Struct.new(:verb, :body_data, :headers).new(verb, body_data, headers)
       end
     end
   end
