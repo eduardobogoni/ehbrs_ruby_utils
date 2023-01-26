e2e30e0e97a1706c5270bf6dba5a2de80ebc6da6
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-26 08:13:52 -0300

    EhbrsRubyUtils::Videos::Opensubtitles::Processors::Episode.

---
+++ lib/ehbrs_ruby_utils/videos/opensubtitles/processors/episode.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'aranha/default_processor'
+require 'ehbrs_ruby_utils/videos/opensubtitles/parsers/episode'
+require 'ehbrs_ruby_utils/videos/opensubtitles/processors/subtitle'
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Videos
+    module Opensubtitles
+      module Processors
+        class Episode < ::Aranha::DefaultProcessor
+          enable_simple_cache
+          enable_speaker
+
+          def perform
+            infov source_uri, subtitles.count
+            subtitles.each(&:perform)
+            next_page.if_present(&:perform)
+          end
+
+          private
+
+          def next_page_uncached
+            data.fetch(:next_page_href)
+              .if_present { |v| self.class.new(source_uri + v, extra_data) }
+          end
+
+          def subtitle_uri(subtitle_data)
+            source_uri + subtitle_data.fetch(:href)
+          end
+
+          def subtitles_uncached
+            data.fetch(:subtitles).map do |subtitle_data|
+              ::EhbrsRubyUtils::Videos::Opensubtitles::Processors::Subtitle.new(
+                subtitle_uri(subtitle_data), extra_data
+              )
+            end + next_page.if_present([], &:subtitles)
+          end
+        end
+      end
+    end
+  end
+end
