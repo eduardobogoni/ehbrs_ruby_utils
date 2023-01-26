ab273805504d6c207bd5b04cbba98cfa36dff8cf
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-26 09:54:22 -0300

    EhbrsRubyUtils::Videos::Opensubtitles::Parsers.

---
+++ lib/ehbrs_ruby_utils/videos/opensubtitles/parsers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Videos
+    module Opensubtitles
+      module Parsers
+        require_sub __FILE__
+      end
+    end
+  end
+end
