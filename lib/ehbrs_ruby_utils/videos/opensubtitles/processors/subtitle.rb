85799d9812b9cadf28cd91944ee489f233d17f93
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-01-26 08:13:53 -0300

    EhbrsRubyUtils::Videos::Opensubtitles::Processors::Subtitle.

---
+++ lib/ehbrs_ruby_utils/videos/opensubtitles/processors/subtitle.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'aranha/default_processor'
+require 'eac_fs/cached_download'
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Videos
+    module Opensubtitles
+      module Processors
+        class Subtitle < ::Aranha::DefaultProcessor
+          enable_simple_cache
+          enable_speaker
+
+          def perform
+            infov '  * ', source_uri
+            assert_download
+          rescue ::RuntimeError => e
+            error(e)
+          end
+
+          private
+
+          def assert_download
+            cached_download.assert
+          end
+
+          def cached_download_uncached
+            ::EacFs::CachedDownload.new(source_uri, fs_cache)
+          end
+
+          def fs_object_id
+            source_uri.to_s.parameterize
+          end
+        end
+      end
+    end
+  end
+end
