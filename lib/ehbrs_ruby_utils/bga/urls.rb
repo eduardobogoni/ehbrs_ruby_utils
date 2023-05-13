0a4477c678e822abcaec5cfa57187cd8bc77c16f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-13 02:46:53 -0300

    EhbrsRubyUtils::Bga::Urls.

---
+++ lib/ehbrs_ruby_utils/bga/urls.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'aranha/selenium/session'
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Bga
+    module Urls
+      common_concern
+
+      module InstanceMethods
+        ROOT_URL = 'https://en.boardgamearena.com'
+
+        # @param suffix [String]
+        # @return [Addressable::URI]
+        def build_url(suffix)
+          root_url + suffix
+        end
+
+        # @return [Addressable::URI]
+        def root_url
+          ROOT_URL.to_uri
+        end
+
+        # @return [Addressable::URI]
+        def table_url(table_id)
+          build_url("/table?table=#{table_id}")
+        end
+      end
+
+      extend InstanceMethods
+    end
+  end
+end
