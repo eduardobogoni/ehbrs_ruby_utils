bb9072e9098774611a445f1110639ade01d031ea
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-22 17:41:07 -0300

    EhbrsRubyUtils::Bga::Parsers::GameStats#{rank,score}: corrige.

--- lib/ehbrs_ruby_utils/bga/parsers/game_stats/players.rb
+++ lib/ehbrs_ruby_utils/bga/parsers/game_stats/players.rb
@@ -10,10 +10,10 @@ module EhbrsRubyUtils
         class Players < ::Aranha::Parsers::Html::ItemList
           ITEMS_XPATH = '//div[@class = "simple-score-entry"]'
 
-          field :rank, :integer, './div[@class = "rank"]'
+          field :rank, :integer_optional, './div[@class = "rank"]'
           field :id, :integer, './div[@class = "name"]/a/@href'
           field :name, :string, './div[@class = "name"]/a/text()'
-          field :score, :integer, './div[@class = "score"]'
+          field :score, :integer_optional, './div[@class = "score"]'
 
           def items_xpath
             ITEMS_XPATH
