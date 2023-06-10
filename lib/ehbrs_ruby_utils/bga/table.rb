4bd65e42bf71b2eefd8614d81ab1178769e21f4e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-06-10 00:19:07 -0300

    EhbrsRubyUtils::Bga::Table: corrige requires.

--- lib/ehbrs_ruby_utils/bga/table.rb
+++ lib/ehbrs_ruby_utils/bga/table.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'ehbrs_ruby_utils/bga/parsers/table'
 require 'ehbrs_ruby_utils/bga/parsers/table/options'
 require 'ehbrs_ruby_utils/bga/urls'
 require 'eac_ruby_utils/core_ext'
