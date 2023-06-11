ae1bf058610df98d9aaffb55d15085539c39e3ae
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-06-11 02:06:34 -0300

    EhbrsRubyUtils::Bga::Game.

---
+++ lib/ehbrs_ruby_utils/bga/game.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Bga
+    class Game
+      common_constructor :code
+
+      # @param suffix [String]
+      # @return [EhbrsRubyUtils::Bga::Game::Image]
+      def image(suffix)
+        ::EhbrsRubyUtils::Bga::Game::Image.new(self, suffix)
+      end
+
+      {
+        publisher: '/publisher/0.png',
+        banner_medium: '/banner/default_500.jpg',
+        banner_large: '/banner/default.jpg',
+        box_small: '/box/en_75.png',
+        box_medium: '/box/en_180.png',
+        box_large: '/box/en_280.png',
+        title_medium: '/title/en_500.png',
+        title_large: '/title/en_2000.png'
+      }.each do |k, suffix|
+        define_method "#{k}_image" do
+          image(suffix)
+        end
+      end
+
+      # @param nth [Integer]
+      # @return [Addressable::URI]
+      def display_image(nth)
+        image("/display/#{nth}.jpg")
+      end
+
+      require_sub __FILE__
+    end
+  end
+end
---
+++ lib/ehbrs_ruby_utils/bga/game/image.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Bga
+    class Game
+      class Image
+        common_constructor :game, :suffix
+
+        IMAGE_URL_PREFIX = 'https://x.boardgamearena.net/data/gamemedia/'
+
+        # @return [String]
+        def fs_object_id
+          [game.code, suffix].map(&:parameterize)
+        end
+
+        # @param suffix [String]
+        # @return [Pathname]
+        def local_path
+          download_to_cache unless fs_cache.stored?
+          fs_cache.content_path.to_pathname
+        end
+
+        # @return [Addressable::URI]
+        def url
+          "#{IMAGE_URL_PREFIX}#{game.code}#{suffix}".to_uri
+        end
+
+        private
+
+        # @return [void]
+        def download_to_cache
+          fs_cache.send(:assert_directory_on_path)
+          ::EacEnvs::Http::Request.new.url(url).response.write_body(fs_cache.content_path)
+        end
+      end
+    end
+  end
+end
