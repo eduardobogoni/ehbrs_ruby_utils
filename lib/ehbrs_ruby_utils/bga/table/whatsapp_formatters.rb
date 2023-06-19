2c2ad18dbf764dabccdc9ebb09c035c250245911
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-06-18 22:38:55 -0300

    EhbrsRubyUtils::Bga::Table::WhatsappFormatters.

---
+++ lib/ehbrs_ruby_utils/bga/table/whatsapp_formatters.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Bga
+    class Table
+      module WhatsappFormatters
+        require_sub __FILE__
+      end
+    end
+  end
+end
