3cb2042ced0c4a3a3d75143c8439dbb071811c0e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-25 20:39:30 -0300

    EhbrsRubyUtils::Vg::Ps2::IsoFile.

---
+++ lib/ehbrs_ruby_utils/vg/ps2/iso_file.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'ehbrs_ruby_utils/fs/iso_9660_file'
+
+module EhbrsRubyUtils
+  module Vg
+    module Ps2
+      class IsoFile < ::EhbrsRubyUtils::Fs::Iso9660File
+        CODE_PATTERN = /[a-zA-Z]{4}_[0-9]{3}\.[0-9]{2}/.freeze
+        CODE_PARSER = CODE_PATTERN.to_parser(&:to_s)
+
+        # @return [String]
+        def code
+          list.lazy.map { |line| CODE_PARSER.parse(line) }.find(&:present?)
+        end
+
+        # @return [String]
+        def basename_nocode_noext
+          path.basename_noext.to_path.gsub(/\A#{::Regexp.quote(code)}/, '')
+        end
+
+        # @return [Path]
+        def target_path
+          path.basename_sub do |_b|
+            "#{code}.#{path_basename}#{DEFAULT_EXTNAME}"
+          end
+        end
+
+        # @return [Boolean]
+        def valid?
+          super && code.present?
+        end
+
+        private
+
+        # @return [String]
+        def path_basename
+          path.basename_noext.to_path.gsub(/\A#{::Regexp.quote(code)}/, '')
+            .gsub(/[^0-9a-zA-Z]+/, '_').gsub(/\A_+/, '').gsub(/_+\z/, '')
+        end
+      end
+    end
+  end
+end
