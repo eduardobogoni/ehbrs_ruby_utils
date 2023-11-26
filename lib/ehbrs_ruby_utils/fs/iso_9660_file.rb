c5125a53e3d8fbd7e34a24133a5985bedd4c96ca
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-25 21:59:41 -0300

    EhbrsRubyUtils::Fs::Iso9660File.

---
+++ lib/ehbrs_ruby_utils/fs/iso_9660_file.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'ehbrs_ruby_utils/executables'
+
+module EhbrsRubyUtils
+  module Fs
+    class Iso9660File
+      DEFAULT_EXTNAME = '.iso'
+
+      common_constructor :path do
+        self.path = path.to_pathname
+      end
+
+      # @return [Array<String>]
+      def list
+        isoinfo_command('-f').execute!.each_line
+      end
+
+      # @param command_args [Array<String>]
+      # @return [EacRubyUtils::Envs::Command]
+      def isoinfo_command(*command_args)
+        ::EacRubyUtils::Envs.local.command('isoinfo', '-i', path, *command_args)
+      end
+
+      # @return [Boolean]
+      def valid?
+        isoinfo_command.execute.fetch(:exit_code).zero?
+      end
+    end
+  end
+end
