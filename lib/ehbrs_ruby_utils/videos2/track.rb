5f95d321de8d2f607e8531451094d9f4ded80688
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-01-16 12:09:53 -0300

    EhbrsRubyUtils::Videos2::Track.to_s: remove.

--- lib/ehbrs_ruby_utils/videos2/track.rb
+++ lib/ehbrs_ruby_utils/videos2/track.rb
@@ -6,11 +6,6 @@ require 'ehbrs_ruby_utils/videos/stream'
 module EhbrsRubyUtils
   module Videos2
     class Track < ::SimpleDelegator
-      def to_s
-        "[#{codec_type}(#{index}): #{codec_name}/#{language || '-'}#{extra.if_present('') do |v|
-                                                                       " | #{v}"
-                                                                     end}]"
-      end
     end
   end
 end
