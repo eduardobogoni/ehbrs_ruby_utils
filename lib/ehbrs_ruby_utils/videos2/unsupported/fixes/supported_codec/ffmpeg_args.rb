e86947af257e3bc7f09897fb56084c3567f35fa2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-01-16 17:33:43 -0300

    EhbrsRubyUtils::Videos2::Unsupported::Fixes::SupportedCodec::FfmpegArgs: refatora.

--- lib/ehbrs_ruby_utils/videos2/unsupported/fixes/supported_codec/ffmpeg_args.rb
+++ lib/ehbrs_ruby_utils/videos2/unsupported/fixes/supported_codec/ffmpeg_args.rb
@@ -6,13 +6,18 @@ module EhbrsRubyUtils
       module Fixes
         class SupportedCodec
           class FfmpegArgs
-            TRACK_TYPE_OPTIONS = {
-              audio: '-acodec',
-              video: '-vcodec',
-              subtitle: '-scodec'
+            FORMATS = {
+              i: :index,
+              f: :target_codec,
+              t: :codec_type_letter
             }.freeze
 
-            TRACK_TYPE_FIX_CODECS = {
+            SOURCE_COMMON_ARGS = ['-%tcodec:%i', '%f'].freeze
+            SOURCE_AUDIO_ARGS = SOURCE_COMMON_ARGS
+            SOURCE_SUBTITLE_ARGS = SOURCE_COMMON_ARGS
+            SOURCE_VIDEO_ARGS = SOURCE_COMMON_ARGS
+
+            TARGET_CODECS = {
               audio: 'aac',
               video: 'libx264',
               subtitle: 'ass'
@@ -23,19 +28,37 @@ module EhbrsRubyUtils
 
             # @return [Array<String>]
             def result
-              ["#{track_codec_option_by_type}:#{track.index}", track_codec_fix_by_type]
+              source_args_by_codec_type.map { |arg| target_arg(arg) }
             end
 
             private
 
             # @return [String]
-            def track_codec_option_by_type
-              TRACK_TYPE_OPTIONS.fetch(track.codec_type.to_s.underscore.to_sym)
+            def codec_type_letter
+              track.codec_type.to_s[0].downcase
             end
 
             # @return [String]
-            def track_codec_fix_by_type
-              TRACK_TYPE_FIX_CODECS.fetch(track.codec_type.to_s.underscore.to_sym)
+            def index
+              track.index.to_s
+            end
+
+            # @return [Array<String>]
+            def source_args_by_codec_type
+              self.class.const_get("SOURCE_#{track.codec_type.to_s.underscore.upcase}_ARGS")
+            end
+
+            # @param arg [String]
+            # @return [String]
+            def target_arg(arg)
+              FORMATS.inject(arg) do |a, e|
+                a.gsub("%#{e.first}", send(e.last))
+              end
+            end
+
+            # @return [String]
+            def target_codec
+              TARGET_CODECS.fetch(track.codec_type.to_s.underscore.to_sym)
             end
           end
         end
