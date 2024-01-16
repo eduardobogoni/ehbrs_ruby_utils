d6e8a38d376bc292a7a1e8ec1108b340e83decb2
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-01-16 15:38:06 -0300

    EhbrsRubyUtils::Videos2::File.time_to_seconds: remove não utilizado.

--- lib/ehbrs_ruby_utils/videos2/file.rb
+++ lib/ehbrs_ruby_utils/videos2/file.rb
@@ -9,25 +9,6 @@ module EhbrsRubyUtils
     class File < ::EhbrsRubyUtils::Videos::Container
       enable_simple_cache
 
-      TIME_PATTERN = /(\d+):(\d{2}):(\d{2})(?:\.(\d+))/.freeze
-
-      class << self
-        def time_to_seconds(time)
-          m = TIME_PATTERN.match(time)
-          raise "Time pattern not find in \"#{time}\"" unless m
-
-          hmsf_to_seconds(m[1], m[2], m[3], m[4])
-        end
-
-        private
-
-        def hmsf_to_seconds(hour, minute, second, float_part)
-          r = (hour.to_f * 3600) + (minute.to_f * 60) + second.to_f
-          r += float_part.to_f / (10**float_part.length) if float_part
-          r
-        end
-      end
-
       private
 
       def tracks_uncached
