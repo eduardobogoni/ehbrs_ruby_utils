f0859b63da14f8d9f9dec5b5545bd7ef26ad767f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-02-08 01:22:32 -0300

    EhbrsRubyUtils::Bga::Session::PlayerTablesOnGameInProgress: adapta a "12/02/2024".

--- lib/ehbrs_ruby_utils/bga/session/player_tables_on_game_in_progress.rb
+++ lib/ehbrs_ruby_utils/bga/session/player_tables_on_game_in_progress.rb
@@ -13,6 +13,8 @@ module EhbrsRubyUtils
         enable_simple_cache
         common_constructor :session, :player_id
 
+        WAIT_FOR_RESULT_SECONDS = 60
+
         def result
           navigate_to_page
           result_data
@@ -25,13 +27,27 @@ module EhbrsRubyUtils
 
         private
 
+        def current_data
+          ::EhbrsRubyUtils::Bga::Parsers::GameInProgress.from_content(session.current_source).data
+        end
+
         # @return [Array<Hash>]
         def result_data
-          ::EhbrsRubyUtils::Bga::Parsers::GameInProgress.from_content(session.current_source).data
+          cr = nil
+          session.wait(WAIT_FOR_RESULT_SECONDS).until do
+            scroll_down
+            cr = current_data
+            cr.fetch(:tables).count == cr.fetch(:table_count)
+          end
+          cr.fetch(:tables)
         end
 
         def navigate_to_page
           session.navigate.to url
+        end
+
+        def scroll_down
+          session.execute_script('window.scrollBy(0, 90)')
         end
       end
     end
