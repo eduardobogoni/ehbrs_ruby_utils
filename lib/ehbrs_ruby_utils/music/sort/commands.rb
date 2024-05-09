4cd6b22c2a847016bcac00cef518c719c437ab46
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-09 16:51:00 -0300

    EhbrsRubyUtils::Music::Sort::Commands.

---
+++ lib/ehbrs_ruby_utils/music/sort/commands.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'ehbrs_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Music
+    module Sort
+      module Commands
+        require_sub __FILE__
+      end
+    end
+  end
+end
