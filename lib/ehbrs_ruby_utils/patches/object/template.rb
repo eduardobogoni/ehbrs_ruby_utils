9f30152061b7864c501ea236aa08dc2c063e322e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-05-09 15:42:17 -0300

    EhbrsRubyUtils::Patches::Object::Template: corrige.

--- lib/ehbrs_ruby_utils/patches/object/template.rb
+++ lib/ehbrs_ruby_utils/patches/object/template.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
 require 'eac_templates/patches/object/template'
-
-EacTemplates::Searcher.default.included_paths <<
-  File.expand_path('../../../../template', __dir__)
+require 'eac_templates/sources/from_all_gems'
