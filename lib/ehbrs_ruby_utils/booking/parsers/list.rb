68aa36f17708b5d6000ab8033b8c57dfcfa3c8be
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-06-08 12:09:42 -0300

    EhbrsRubyUtils::Booking::Parsers::List.

---
+++ lib/ehbrs_ruby_utils/booking/parsers/list.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'aranha/parsers/html/item_list'
+require 'eac_ruby_utils/core_ext'
+
+module EhbrsRubyUtils
+  module Booking
+    module Parsers
+      class List < ::Aranha::Parsers::Html::ItemList
+        class << self
+          def testid_xpath(value, *suffixes)
+            ([".//*[@data-testid='#{value}']"] + suffixes).join('/')
+          end
+        end
+
+        ITEMS_XPATH = '//*[@data-testid="property-card"]'
+
+        field :name, :string, testid_xpath('title', 'text()')
+        field :href, :string, testid_xpath('title-link', '@href')
+        field :price, :float, testid_xpath('price-and-discounted-price', '/text()')
+        field :tax, :float_optional, testid_xpath('taxes-and-charges', 'text()')
+        field :address, :string, testid_xpath('address', 'text()')
+        field :distance, :distance, testid_xpath('distance', 'text()')
+        field :review_score, :float_optional, testid_xpath('review-score', 'div[1]/text()')
+        field :review_count, :integer, testid_xpath('review-score', 'div[2]/div[2]/text()')
+        field :unit_title, :string_recursive, testid_xpath('recommended-units',
+                                                           '/*[@role="link"]')
+
+        def data
+          { accommodations: items_data, declared_count: declared_count }
+        end
+
+        # @return [Integer]
+        def declared_count
+          node_parser.integer_value(nokogiri, '//h1/text()')
+        end
+
+        def items_xpath
+          ITEMS_XPATH
+        end
+
+        def node_parser
+          r = super
+          r.extend(NodeParserExtra)
+          r
+        end
+
+        module NodeParserExtra
+          DISTANCE_PARSER = /\A(\d+)(?:,(\d+))? (m|km)/.to_parser do |m|
+            r = m[1].to_f + m[2].if_present(0) { |v| v.to_f / 10.pow(v.length) }
+            r /= 1000 if m[3] == 'm'
+            r
+          end
+
+          def distance_value(node, xpath)
+            s = string_value(node, xpath)
+            DISTANCE_PARSER.parse!(s)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/ehbrs_ruby_utils/booking/parsers/list_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'ehbrs_ruby_utils/booking/parsers/list'
+
+RSpec.describe EhbrsRubyUtils::Booking::Parsers::List do
+  include_examples 'source_target_fixtures', __FILE__
+end
