14bfa89c2f53f9d453b6d133eda811e5f24f27a8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-10 22:02:12 -0300

    EhbrsRubyUtils: corrige requires ausentes.

--- lib/ehbrs_ruby_utils.rb
+++ lib/ehbrs_ruby_utils.rb
@@ -6,4 +6,12 @@ EacRubyUtils::RootModuleSetup.perform __FILE__
 module EhbrsRubyUtils
 end
 
+require 'aranha'
 require 'aranha/parsers'
+require 'aranha/selenium'
+require 'avm'
+require 'avm/eac_rails_base0'
+require 'eac_fs'
+require 'eac_rest'
+require 'eac_templates'
+require 'ultimate_lyrics'
