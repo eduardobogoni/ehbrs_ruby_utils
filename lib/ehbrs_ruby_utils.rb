9fcb6cd43026c2752c38009d8e47fb7d5cc68900
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-29 17:49:34 -0300

    EhbrsRubyUtils: refatora com EacRubyBase1::RootModuleSetup.

--- lib/ehbrs_ruby_utils.rb
+++ lib/ehbrs_ruby_utils.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
-
-module EhbrsRubyUtils
+require 'eac_ruby_base1'
+EacRubyBase1::RootModuleSetup.perform __FILE__ do
+  require 'aranha'
+  require 'aranha/parsers'
+  require 'aranha/selenium'
+  require 'avm'
+  require 'avm/eac_rails_base0'
+  require 'eac_fs'
+  require 'eac_rest'
+  require 'eac_templates'
+  require 'ultimate_lyrics'
 end
-
-require 'aranha'
-require 'aranha/parsers'
-require 'aranha/selenium'
-require 'avm'
-require 'avm/eac_rails_base0'
-require 'eac_fs'
-require 'eac_rest'
-require 'eac_templates'
-require 'ultimate_lyrics'
